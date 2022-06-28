157e69df63ec47e7fad0e722e66516da09f079dc
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-06-28 18:07:56 -0300

    requisito 6 em andamento1

---
+++ src/pages/Game.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Game extends React.Component {
+
+  requestQuestions = async () => {
+    const token = localStorage.getItem('token');
+    const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    const questions = await response.json();
+    console.log(questions.category);
+    // this.setState({
+    //   token,
+    // });
+    // localStorage.setItem('token', JSON.stringify(token.token));
+  };
+
+  render() {
+    return (
+      <main>
+        <header>Header</header>
+        <h3 data-testid="question-category">category</h3>
+        <h6 data-testid="question-text">question</h6>
+        <section data-testid="answer-options">
+          <button type="submit" data-testid="correct-answer">Option1</button>
+          <button type="submit" data-testid={ `wrong-answer-` }>Option2</button>
+          <button type="submit" data-testid={ `wrong-answer-` }>Option3</button>
+          <button type="submit" data-testid={ `wrong-answer-` }>Option4</button>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Game;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 class Login extends React.Component {
   state = {
     nome: '',
