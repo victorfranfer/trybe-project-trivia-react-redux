c393386ad5a43fb536572eddd6e9384155ab9a56
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-06-30 14:52:11 -0300

    Requisito 13 feito

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const { score } = this.props;
+    const answerThreshold = 3;
     return (
-      <Header />
+      <main>
+        <Header />
+        <h3 data-testid="feedback-text">
+          { score < answerThreshold ? 'Could be better...' : 'Well done!' }
+        </h3>
+      </main>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
